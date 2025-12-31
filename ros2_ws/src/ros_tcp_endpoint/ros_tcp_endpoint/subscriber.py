9bbc720712edc97e32c73959c4f71e807a928df9
LSW <lkzaxx.work@gmail.com>
2025-12-31 11:11:45 +0800

    add debug code

--- ros2_ws/src/ros_tcp_endpoint/ros_tcp_endpoint/subscriber.py
+++ ros2_ws/src/ros_tcp_endpoint/ros_tcp_endpoint/subscriber.py
@@ -61,6 +61,9 @@ class RosSubscriber(RosReceiver):
             self.msg: The deserialize message
 
         """
+        # 調試日誌
+        print(f"[RosSubscriber] 收到訊息: {self.topic}, 正在轉發給 Unity...")
+        
         self.tcp_server.send_unity_message(self.topic, data)
         return self.msg
 
